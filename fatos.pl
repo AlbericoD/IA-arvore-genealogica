2b6bac0de16b7cecca780d555b09d60360927be6
AlbericoD <a.diasbarreto@icloud.com>
2020-06-13 16:05:44 -0300

    adicionado fatos e regras iniciais.

---
+++ fatos.pl
@@ -0,0 +1,36 @@
+% FATOS
+/*
+ * FATOS 1 - HOMENS DA FAMILIA 
+ sexo masculino:- JOSÉ, THIAGO, JOÃO, LUCAS, ANTÔNIO.
+*/
+homem(jose).
+homem(thiago).
+homem(joao).
+homem(lucas).
+homem(antonio).
+/*
+ * FATOS 2 - MULHERES DA FAMILIA 
+ sexo feminino:- MARIA, MANOELA, ELIZA.
+*/
+mulher(maria).
+mulher(manoela).
+mulher(eliza).
+/*
+ * REGRA 3 - GRAU DE PARENTESCO
+ * Filhos de jose e maria: Thiago e Lucas
+ * Filhos de thiago e manoela: João
+ * Filhos de lucas: Antônio
+*/
+pais(thiago, jose).
+pais(thiago, maria).
+
+pais(lucas, jose).
+pais(lucas, maria).
+
+pais(joao, thiago).
+pais(joao, manoela).
+
+pais(antonio, lucas).
+pais(eliza, lucas).
+
+% REGRAS
